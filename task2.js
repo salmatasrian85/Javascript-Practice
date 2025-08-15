723fae168adb874bed95aa2342782868752778e2
salmatasrian85 <salmatasrian697@gmail.com>
2025-08-15 21:21:45 +0600

    task3

---
+++ task2.js
@@ -0,0 +1,9 @@
+//Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+let Mathematics = 75.25;
+let Biology = 65;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.50;
+
+let averageMark = (Mathematics + Biology + Chemistry + Physics + Bangla )/ 5;
+console.log(averageMark.toFixed(2));
---
+++ task3.js
@@ -0,0 +1,5 @@
+let x = 119;
+let y = 5;
+
+remainder = x%y;
+console.log("Remainder : " + remainder);
