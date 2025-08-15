bd5c735be796db9c8e19113df60f4ee7b46167a5
tasrian85 <salmatasrian85@gmail.com>
2025-08-15 20:54:01 +0600

    task1

---
+++ task1.js
@@ -0,0 +1,9 @@
+//You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+let money = 100;
+let orangeCost = 30;
+let applecost = 20;
+
+let totalcost = orangeCost + applecost ;
+let returnMoney = money - totalcost;
+console.log(returnMoney);
