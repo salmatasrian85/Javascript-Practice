e051b2586f006fbbe024368fa7e1efa98631177f
salmatasrian85 <salmatasrian697@gmail.com>
2025-09-03 22:35:47 +0600

    changes

---
+++ basic practice/objects/practice.js
@@ -0,0 +1,10 @@
+
+
+const person = new Object({
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue" 
+});
+
+console.log(person.firstName + " is " + person.age + " years old.");
