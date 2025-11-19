50bb40b1a7795b967d51a97c73e4b4044ed6de16
salmatasrian85 <salmatasrian697@gmail.com>
2025-11-19 21:27:33 +0600

    update

---
+++ basic practice/String and array/StringInterpolation.js
@@ -0,0 +1,7 @@
+const myPet = 'armadillo';
+console.log(`I own a pet ${myPet}.`);
+
+
+const myName = 'Salma';
+const myCity = 'Cox';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`)
